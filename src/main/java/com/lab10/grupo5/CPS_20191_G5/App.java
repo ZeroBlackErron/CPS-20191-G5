d5204033e9905fab39bd99f114d4ccf505486b08
Anthony Rosado <arosado-26@hotmail.com>
2019-05-10 16:03:23 -0500

    Initial commit

---
+++ src/main/java/com/lab10/grupo5/CPS_20191_G5/App.java
@@ -0,0 +1,13 @@
+package com.lab10.grupo5.CPS_20191_G5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
